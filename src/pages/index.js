d5e8fe545e862951a4047473023758d4cea78fc9
Sudheer Sudheer <mahima.kilaru@soulpageit.com>
2020-03-21 14:05:29 +0530

    sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/post/">Post</Link>
+    <Link to="/post/">Post kkhkjhkhkhk</Link>
   </Layout>
 )
 
